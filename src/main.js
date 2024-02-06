68f1fbb837122a87d3ce321a3f262c65eca2432f
sui-tyan <mallari23adrian@gmail.com>
2024-02-06 17:16:11 +0800

    Update: Added Sessions
    Utilized express session to handle user sessions

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 import express from "express";
+import session from "express-session";
+import passport from "passport";
 import organization_route from "./routes/organization_route.js";
+import member_route from "./routes/member_route.js";
+import authenticate_route from "./routes/authenticate_route.js";
+import "./utils/auth.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
+app.use(authenticate_route);
 app.use(organization_route);
+app.use(member_route);
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
