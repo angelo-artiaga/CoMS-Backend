9ae923fcf44b9d39a2863027558f826574f4965f
sui-tyan <mallari23adrian@gmail.com>
2024-02-12 17:14:50 +0800

    Removed: Passport
    Moved OAuth2 on frontend

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import express from "express";
 import session from "express-session";
-import passport from "passport";
 import organization_route from "./routes/organization_route.js";
 import member_route from "./routes/member_route.js";
 import authenticate_route from "./routes/authenticate_route.js";
-import "./utils/auth.js";
+import user_route from "./routes/user_route.js";
 import db from "./database/db.js";
 
 const app = express();
@@ -16,12 +15,11 @@ app.use(
     saveUninitialized: false,
   })
 );
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.json());
 app.use(authenticate_route);
 app.use(organization_route);
 app.use(member_route);
+app.use(user_route);
 app.get("/", (req, res) => {
   db("users")
     .select("*")
--- src/routes/authenticate_route.js
+++ src/routes/authenticate_route.js
@@ -1,24 +1,10 @@
 import express from "express";
-import passport from "passport";
 import {
   createSession,
   destroySession,
 } from "../controllers/authenticate_controller.js";
 import isAuthenticated from "../utils/isAuth.js";
 const router = express.Router();
-
-router.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["email", "profile"] })
-);
-
-router.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    successRedirect: "/authorized",
-    failureRedirect: "/unauthorized",
-  })
-);
 
 router.get("/authorized", isAuthenticated, createSession);
 router.get("/logout", isAuthenticated, destroySession);
