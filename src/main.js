55bbbbc170eb6a3825bf42b37c4ab9ea49529924
angelo-artiaga <angelo.artiaga@gmail.com>
2024-04-01 09:23:28 +0800

    add company controllers

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import organization_route from "./routes/organization_route.js";
 import member_route from "./routes/member_route.js";
 import authenticate_route from "./routes/authenticate_route.js";
 import user_route from "./routes/user_route.js";
+import companyRoute from "./routes/companyRoutes.js";
 import db from "./database/db.js";
 
 const app = express();
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(authenticate_route);
 app.use(organization_route);
 app.use(member_route);
+app.use(companyRoute);
 app.use(user_route);
 app.get("/", (req, res) => {
   db("users")
