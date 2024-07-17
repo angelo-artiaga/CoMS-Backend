3315ab59d8e61cf95d33cd47af28ffa65c9228d9
BenjiePecson <161810296+BenjiePecson@users.noreply.github.com>
2024-07-17 15:34:43 +0800

    Created a function for changing letter header

--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -9,9 +9,9 @@ import {
   deleteCompany,
   getCompany,
   changeStatus,
-  getPaginateCompany
+  getPaginateCompany,
+  updateLetterHeader,
 } from "../controllers/companyControllers.js";
-
 
 // router.route("/company/").post(createCompany).get(getAllCompany);
 router.get("/company", getAllCompany);
@@ -19,6 +19,7 @@ router.get("/paginateCompany", getPaginateCompany);
 router.post("/company", uploadMulter.single("logo"), createCompany);
 router.patch("/company/:id", uploadMulter.single("logo"), updateCompany);
 router.patch("/company/:id/changeStatus", changeStatus);
+router.patch("/company/:id/updateLetterHeader", updateLetterHeader);
 
 router
   .route("/company/:id")
