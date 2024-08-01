8fb2c867f8ca5656917fc1ebbb46f5f25378b88f
BenjiePecson <161810296+BenjiePecson@users.noreply.github.com>
2024-08-01 13:53:00 +0800

    Fixed local db connection

--- src/database/knexfile.cjs
+++ src/database/knexfile.cjs
@@ -6,17 +6,7 @@
 module.exports = {
   development: {
     client: "postgresql",
-    // connection: process.env.LOCAL_DB_CONN_STRING,
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      ssl: {
-        rejectUnauthorized: false, // or true based on your SSL requirements
-      },
-    },
+    connection: process.env.LOCAL_DB_CONN_STRING,
     pool: {
       min: 2,
       max: 10,
