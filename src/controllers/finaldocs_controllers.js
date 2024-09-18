68e0bfdc5344e1f656e6fb81ee0078cd2036435c
BenjiePecson <161810296+BenjiePecson@users.noreply.github.com>
2024-09-18 18:02:28 +0800

    Added: Company Warnings

--- src/controllers/finaldocs_controllers.js
+++ src/controllers/finaldocs_controllers.js
@@ -59,4 +59,44 @@ const addFinalDocs = async (req, res) => {
   }
 };
 
-export { getAllFinalDocs, addFinalDocs };
+const checkWarnings = async (req, res) => {
+  const companyId = req.params.companyId;
+  try {
+    const company = await db("companies")
+      .select("*")
+      .where("companyId", companyId)
+      .first();
+    const gis_records = await db("records")
+      .select("*")
+      .where("companyId", companyId);
+
+    const gis_records_current_year = gis_records.filter(
+      (record) =>
+        record.draftingInput.year == new Date().getFullYear() &&
+        record.status == "Completed"
+    );
+
+    const gdrivefolders = JSON.parse(company.gdrivefolders);
+    let MC28Form = "";
+
+    if (gdrivefolders != null) {
+      if (
+        gdrivefolders.MC28Form != undefined &&
+        gdrivefolders.MC28Form != null &&
+        gdrivefolders.MC28Form != ""
+      ) {
+        MC28Form = gdrivefolders.MC28Form;
+      }
+    }
+
+    res.status(200).json({
+      MC28Form,
+      current_year_GIS_count: gis_records_current_year.length,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: "Internal Server Error", err: e });
+  }
+};
+
+export { getAllFinalDocs, addFinalDocs, checkWarnings };
