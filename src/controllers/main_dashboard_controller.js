52335604fb7f8dd3f326e12c545cf824f74d2e37
Anthony Cabulang <anthonycabulang@Fullsuite-Macbook.local>
2024-10-09 10:44:11 +0800

    second commit, pushing all the status for main dasboard in the backend

--- src/controllers/main_dashboard_controller.js
+++ src/controllers/main_dashboard_controller.js
@@ -3,7 +3,13 @@ import db from "../database/db.js";
 export const viewMainDashboard = async (req, res) => {
 
     const notarization = await db('records').select("*").where({status : 'For Notarization'});
-    console.log(notarization);
-    res.status(200).json({notarization});
+    const completed = await db('records').select("*").where({status : 'Completed'});
+    const pending = await db('records').select("*").where({status : 'Pending for Approval'});
+    const approved = await db('records').select("*").where({status : 'Approved'});
+    const drafted = await db('records').select("*").where({status : 'Saved as Draft'});
+    const routed = await db('records').select("*").where({status : 'Routed for Signature'});
+    const filed = await db('records').select("*").where({status: "Filed with SEC"});
+
+    res.status(200).json({notarization, completed, pending, approved, drafted, routed, filed});
 };
 
